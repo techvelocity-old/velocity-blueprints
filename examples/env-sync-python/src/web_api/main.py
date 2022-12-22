822e742fed54bfb357bd424d84e35c6c8e904849
Jeff Vincent <34778075+jeff-vincent@users.noreply.github.com>
2022-12-22 07:57:44 -0800

    Add env-sync-python example (#21)
    * Add env-sync-python example
    
    * Add new lines

---
+++ examples/env-sync-python/src/web_api/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import  HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+    return HTMLResponse("<h1> Velocity Env Sync Example </h1>")
